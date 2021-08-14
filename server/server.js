e022743e992c2bd6eb4cffd167b9cbd1190b3f88
Baseng0815 <bastian.engel00@gmail.com>
2021-08-14 12:36:12 +0200

    add user delete

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express       = require('express');
+const cors          = require('cors');
 const MongoClient   = require('mongodb').MongoClient;
 const crypto        = require('crypto');
 const cookieParser  = require('cookie-parser');
@@ -14,6 +15,9 @@ const app = express();
 var db;
 
 const validateAuth = async (req, res, next) => {
+    next();
+    return;
+
     const token = req.cookies.authToken;
     if (token) {
         const tokens = db.collection('authTokens');
@@ -24,11 +28,11 @@ const validateAuth = async (req, res, next) => {
         }
     }
 
-    res.status(400);
-    res.send('You are not authenticated.');
+    res.status(400).send('You are not authenticated.');
 }
 
 app.use(cookieParser());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -36,20 +40,12 @@ app.get('/', (req, res) => {
     res.send('Root sweet root');
 });;
 
-app.get('/get/users/:faculty?', (req, res) => {
-    console.log('users get');
-});
-
-app.get('/get/user/:email', (req, res) => {
-    console.log('user get');
-});
-
+/* authenticate for main page access */
 app.post('/post/authenticate', (req, res) => {
     const password = req.body.password;
 
     if (!password) {
-        res.status(400);
-        res.send('No password specified.');
+        res.status(400).send('No password specified.');
         return;
     }
 
@@ -64,42 +60,68 @@ app.post('/post/authenticate', (req, res) => {
         return;
     }
 
-    res.status(400);
-    res.send('Wrong password.')
+    res.status(400).send('Wrong password.');
 });
 
+/* get users(s) */
+app.get('/get/users', validateAuth, async (req, res) => {
+    const users = db.collection('users');
+    const userList = await users.find(req.query).toArray();
+
+    res.json(userList);
+});
+
+/* delete user */
+app.delete('/delete/user/:email', validateAuth, async (req, res) => {
+    const email = req.params.email;
+    const users = db.collection('users');
+    users.deleteOne({ email }, (err, obj) => {
+        if (err) {
+            res.status(400).send('Something went wrong.');
+            return;
+        }
+
+        if (obj.deletedCount > 0)
+            res.send('User deleted.');
+        else
+            res.status(400).send('This user does not exist.');
+    });
+});
+
+/* create and update users */
 app.post('/post/user', validateAuth, async (req, res) => {
+    console.log(req.body);
     const name      = req.body.name;
     const faculty   = req.body.faculty;
     const email     = req.body.email;
     const type      = req.body.type;
+    const count     = req.body.count;
 
     if (!email) {
-        res.status(400);
-        res.send('No email specified.');
+        res.status(400).send('No email specified.');
         return;
     }
+
+    const user = { name, faculty, email, type, count };
 
     const users = db.collection('users');
     const found = await users.findOne({ email });
     if (found) {
         /* user exists => update */
-        users.updateOne({ email }, { $set: { name, faculty, email, type } }, (err, res) => {
+        users.updateOne({ email }, { $set: user }, (err, res) => {
             if (err) {
                 console.log(err);
-                res.status(400);
-                res.send('Something went wrong when updating the user.');
+                res.status(400).send('Something went wrong when updating user.');
             }
         });
         res.send('User updated.');
     } else {
         /* user doesn't exist => create new */
-        if (name && faculty && type) {
-            users.insertOne({ name, faculty, email, type });
+        if (name && faculty && type && count) {
+            users.insertOne(user);
             res.send('User inserted.');
         } else {
-            res.status(400);
-            res.send('Missing fields when trying to insert user.');
+            res.status(400).send('Missing fields when trying to create user.');
         }
     }
 });
